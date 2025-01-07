9faa599c22f77080f79a0604f485452f122ee489
Fabrice Werger <fabricewerger@icloud.com>
2025-01-07 13:24:24 +0100

    added next 15 caching demo

---
+++ src/app/next/store/2/page.tsx
@@ -0,0 +1,14 @@
+import { getProduct2, getPrice2 } from "../../db";
+
+export default async function ProductPage2() {
+  const description = await getProduct2();
+  const price = await getPrice2();
+
+  return (
+    <div className="p-10 flex flex-col items-center">
+      <div className="text-2xl mb-10">asynchronous example</div>
+      <h1 className="text-blue-500 text-xl">${price}</h1>
+      <p className="text-sm">{description}</p>
+    </div>
+  );
+}
