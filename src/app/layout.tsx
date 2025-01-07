419fd8fcd374da454811f711b210ecd16b087dcf
Mostafa Said <matt_shadowwing@live.com>
2025-01-07 11:42:19 +0100

    removed duplicated page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { fontSans } from "@/lib/fonts";
 import { cn } from "@/lib/utils"
 
 export const metadata: Metadata = {
@@ -17,8 +16,7 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body
           className={cn(
-              "min-h-screen bg-background font-sans antialiased",
-              fontSans.variable
+              "min-h-screen bg-background font-sans antialiased"
           )}
       >
         {children}
